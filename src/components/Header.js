ec1d4ad52c945469073cc80678f0de413d937074
alihoppabb <78644878+alihoppabb@users.noreply.github.com>
2021-06-22 21:39:14 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <header className={'header'}>
+            <div className="container">
+                <img src="https://api.freelogodesign.org/assets/thumb/logo/21888881_400.png" alt=""/>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,23 @@
+const Profile = () => {
+    return (
+        <div className={'content'}>
+            <div className={'content__img'}>
+                <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt="japanStyle"/>
+            </div>
+            <div>
+                ava + description
+            </div>
+
+            <div>
+                my posts
+                <div>new posts</div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
